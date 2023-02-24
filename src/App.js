e1dc93e3f6bd86c5566dcf2e633f49b0edcd061d
meraj2085 <samirmeraj60@gmail.com>
2023-02-24 09:06:24 +0600

    adding sorting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import BasicTable from "./Components/BasicTable";
+import SortingTable from "./Components/SortingTable/SortingTable";
 
 function App() {
   return <div className="App">
-    <BasicTable></BasicTable>
+    {/* <BasicTable></BasicTable> */}
+    <SortingTable></SortingTable>
   </div>;
 }
 
