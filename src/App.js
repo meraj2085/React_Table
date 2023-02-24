55e7dab09239bfa8f996534201b706635ae90387
meraj2085 <samirmeraj60@gmail.com>
2023-02-24 09:29:50 +0600

    adding global filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import BasicTable from "./Components/BasicTable";
 import SortingTable from "./Components/SortingTable/SortingTable";
+import FilteringTable from './Components/Filter/FilteringTable'
 
 function App() {
   return <div className="App">
     {/* <BasicTable></BasicTable> */}
-    <SortingTable></SortingTable>
+    {/* <SortingTable></SortingTable> */}
+    <FilteringTable></FilteringTable>
   </div>;
 }
 
